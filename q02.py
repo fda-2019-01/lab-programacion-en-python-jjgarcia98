205ad069e13883a98bb8e71dcdb748c81c907724
Juan Jose Garcia <50602958+jjgarcia98@users.noreply.github.com>
2019-06-05 22:20:56 -0500

    Update q02.py

--- q02.py
+++ q02.py
@@ -8,3 +8,13 @@
 ## D,6
 ## E,14
 ##
+txt = open('data.csv','r').readlines()
+txt = [row[0:-1]for row in txt]
+txt = [line.replace('\t','')for line in txt]
+c = sorted(set([row[0]for row in txt]))
+s = [row[0]for row in txt]
+aa =0
+for i in c:
+    aa = s.count(i)
+    print(i+','+str(aa))
+    
