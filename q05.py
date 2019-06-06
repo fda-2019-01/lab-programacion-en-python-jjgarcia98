6c247a0691a456a1aa2ebfe271b31fa598053556
Juan Jose Garcia <50602958+jjgarcia98@users.noreply.github.com>
2019-06-05 22:24:52 -0500

    Update q05.py

--- q05.py
+++ q05.py
@@ -7,3 +7,15 @@
 ## D,7,1
 ## E,9,1
 ##
+txt = open('data.csv','r').readlines()
+txt = [row[0:-1]for row in txt]
+txt = [line.replace('\t','')for line in txt]
+c = sorted(set([row[0]for row in txt]))
+s = [[row[0],row[1]]for row in txt]
+aa =0
+for i in c:
+    aa = [row[1] for row in s[:]if row[0]==i]
+    maximo = max(aa)
+    minimo = min(aa)
+    print(i+','+maximo+','+minimo)
+    
