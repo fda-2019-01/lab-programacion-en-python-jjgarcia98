d73add4e44158f65c8739389d1511816dda6c686
Juan Jose Garcia <50602958+jjgarcia98@users.noreply.github.com>
2019-06-05 22:30:31 -0500

    Update q10.py

--- q10.py
+++ q10.py
@@ -15,3 +15,27 @@
 ## jjj,18
 ##
 ##
+txt = open('data.csv','r').readlines()
+txt = [row[0:-1]for row in txt]
+txt = [line.split('\t')for line in txt]
+s = [row[-1]for row in txt]
+s = [line.split(',')for line in s]
+a = []
+b = []
+for i in s:
+    for j in i:
+        a.append(j)
+        
+a = [line.split(':')for line in a]
+for i in a:
+    for j in i:
+        b.append(j)
+d=[]
+for i in b:
+    if len(i) == 3:
+        d.append(i)
+x = sorted(set(d))
+for i in x:
+    aa = d.count(i)
+    print(i+','+str(aa))
+    
