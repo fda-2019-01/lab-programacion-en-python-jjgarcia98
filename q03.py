bde3f0b78f05006ad10fc90267e9a0c03768d067
Juan Jose Garcia <50602958+jjgarcia98@users.noreply.github.com>
2019-06-05 22:22:33 -0500

    Update q03.py

--- q03.py
+++ q03.py
@@ -8,3 +8,29 @@
 ## D,23
 ## E,67
 ##
+m = open('data.csv','r').readlines()
+b=[row[2]for row in m]
+a=([row[0]for row in m])
+x=0
+y=0
+z=0
+s=0
+c=0
+d=0
+for i in a:
+    if i == 'A':
+        y = y + int(b[x])
+    if i == 'B':
+        z = z + int(b[x])
+    if i == 'C':
+        s = s + int(b[x])
+    if i == 'D':
+        c = c + int(b[x])
+    if i == 'E':
+        d = d + int(b[x])                            
+    x=x+1
+print('A'+','+str(y))
+print('B'+','+str(z))
+print('C'+','+str(s))
+print('D'+','+str(c))
+print('E'+','+str(d))
