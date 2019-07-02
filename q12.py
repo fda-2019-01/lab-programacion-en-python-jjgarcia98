4468b3923e5d2aa49f92cf8cdd22b5ea21f3f814
Juan Jose Garcia <50602958+jjgarcia98@users.noreply.github.com>
2019-07-02 11:19:11 -0500

    Update q12.py

--- q12.py
+++ q12.py
@@ -10,3 +10,31 @@
 ## f,110
 ## g,35
 ##
+
+txt = open('data.csv','r').readlines()
+txt = [row[0:-1]for row in txt]
+txt = [line.replace('\t',',')for line in txt]
+txt = [line.split(',')for line in txt]
+m = [[row for row in line if len(row) == 1] for line in txt]
+m = [line[1:] for line in m]
+a = [line[2:] for line in m]
+b = []
+for i in a:
+    for j in i:
+        b = b+[j]
+b = set(b)
+b = list(b)
+b = sorted(b)
+
+d = []
+for i in b:
+    n = [i]
+    c = 0
+    for row in m:
+        if i in row:
+            c = c+int(row[0])
+    n = n+[c]
+    d = d+[n]
+for i in d:
+    u = i[0]+","+str(i[1])
+    print(u)
