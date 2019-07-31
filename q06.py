eb4ddd0d53e925dc2ca990c696e2e98eb7427ebe
Juan Jose Garcia <50602958+jjgarcia98@users.noreply.github.com>
2019-07-31 08:20:46 -0500

    Update q06.py

--- q06.py
+++ q06.py
@@ -13,3 +13,42 @@
 ## iii,2,7
 ## jjj,2,5
 ##
+import glob
+f=glob.glob("data.csv")
+a = open("data.csv", "r").readlines()
+a = [linea[:-1] for linea in a]
+a = [linea.replace("\t", ",") for linea in a]
+a = [linea.split(",") for linea in a]
+a = [linea[5:] for linea in a]
+c=[]
+for linea in a:
+  d = []
+  for x in linea:
+    if len(x)==5:
+      d.append(x)
+  c.append(d)
+n = []
+for i in c:
+  for j in i:
+    n.append(j)
+n = sorted(n)
+m = []
+p=[linea.replace(":",",") for linea in n]
+p=[linea.split(",") for linea in p]
+s = set([linea[0]for linea in p])
+z = []
+for x in s:
+  let=[x]
+  for u in p:
+    if u[0]==x:
+      let.append(u[1])
+  z= z+[let]
+a1= []
+for y in z:
+  a2 = [y[0], min(y), max(y[1:])]
+  a1= a1 + [a2]
+a1 = sorted(a1) 
+for e in a1:
+  l= e[0]+","+e[1]+","+e[2]
+  print(l)
+    
